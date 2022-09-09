6fad5be7bc516c3a246936e70e199a4b71ddc523
ABOLADE <geebolu261@gmail.com>
2022-09-09 09:15:33 -0700

    hmmm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: another program that prints using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+	
+}
