8e914cae29cc74a58fe12c1b50ab7fbbeaa4b001
ABOLADE <geebolu261@gmail.com>
2022-09-09 10:38:19 -0700

    getting a hang of it

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: anoyher challenge
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
