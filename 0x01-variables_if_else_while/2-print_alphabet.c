86f3ff791c60b2feac185576ee5e4efe661444c1
ABOLADE <geebolu261@gmail.com>
2022-09-09 08:15:00 -0700

    zeh now writing codes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: betty wants to be understood further
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char gee;
+
+	for (gee = 'a'; gee <= 'z'; gee++)
+	{
+		putchar(gee);
+	}
+	putchar('\n');
+	return (0);
+}
