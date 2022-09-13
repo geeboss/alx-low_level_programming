abcac2fac7924a5b2869c65d47ce61c717905978
ABOLADE <geebolu261@gmail.com>
2022-09-13 08:13:24 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,17 +2,14 @@
 #include <stdio.h>
 /**
  * _abs - computes the absolute value of an interger
- * An: The int to be operated upon
+ * @n: The int to be operated upon
  * author: Gee boss
  * Return: Always 0 (Success)
  */
 int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 		return (n);
 	else
-	{
-		n *= -1;
-		return (n);
-	}
+		return (-n);
 }
