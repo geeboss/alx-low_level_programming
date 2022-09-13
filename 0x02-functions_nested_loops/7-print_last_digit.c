c5e5c1c33e8aede59274b0bdd3e14161cf10a58a
ABOLADE <geebolu261@gmail.com>
2022-09-13 08:43:43 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digits of any number
+ * @a: the int value to be acted upon
+ * authoer: gee boss
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
