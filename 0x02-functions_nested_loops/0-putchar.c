126876efe56ee3971e34518eb48d9d111adeb81e
ABOLADE <geebolu261@gmail.com>
2022-09-13 03:34:57 -0700

    wonder

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+
+{
+	putchar('_');
+	putchar('P');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+
+}
