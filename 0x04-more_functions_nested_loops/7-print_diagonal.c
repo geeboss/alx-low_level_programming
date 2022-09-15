7cb7093ac753bbfaf613f4922a783d509f0cdd45
ABOLADE <geebolu261@gmail.com>
2022-09-15 16:17:00 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,19 @@
 void print_diagonal(int n)
 {
 	int i = 0;
+	int j;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < 1; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar(92);
-		_putchar('\n');
 	}
 	else
 	{
