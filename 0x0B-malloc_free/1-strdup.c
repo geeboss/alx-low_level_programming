9783ad70ad302acd4c9c12013bcd166fba41f09e
ABOLADE <geebolu261@gmail.com>
2022-10-04 19:08:15 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strlen - count array
  * @s: array of elements
  * Return: 1
  */
 
-char *_strdup(char *s)
+char _strlen(char *s)
 {
 	unsigned int i;
 
@@ -15,5 +16,55 @@ char *_strdup(char *s)
 		i++;
 	}
 
-	return (NULL);
+	return (i);
+}
+
+/**
+ * _strcpy - copy arrays
+ * @src: arrat of elements
+ * @dest: dest array
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strdup - array for prints a string
+ * @str: array of elements
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *dst;
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	size = strlen(str) + 1;
+
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+
+	_strcpy(dst, str);
+	return (dst);
 }
