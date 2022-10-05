08466cbd7b6c548cbb44f8d597da69598ff49c6c
ABOLADE <geebolu261@gmail.com>
2022-10-04 18:53:07 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - count array
+ * @s: array of elements
+ * Return: 1
+ */
+
+char *_strdup(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0') /* count character of string*/
+	{
+		i++;
+	}
+
+	return (NULL);
+}
