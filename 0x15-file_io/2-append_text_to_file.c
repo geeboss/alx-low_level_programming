7544c6f6b9b700d12d26cd98479ef5a518955f1f
ABOLADE <geebolu261@gmail.com>
2022-11-04 21:34:55 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fild = open(filename, O_WRONGLY | O_APPEND);
+	fild = open(filename, O_WRONLY | O_APPEND);
 	if (fild < 0)
 		return (-1);
 
